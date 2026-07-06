29fd98e790f02393160ea62c1af6f41d2b526350
MayankChhatani <mayankchhatani1@gmail.com>
2026-07-06 20:44:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (43.26%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1;
+        long long N =n;
+        if(N == 0){
+            return 1;
+        }
+        if(N < 0){
+            N = -N;
+            x = 1/x;
+        }
+        while(N > 0){
+            if(N % 2 != 0){
+                ans = ans*x;
+                N = N-1;
+            }else{
+                x = x * x;
+                N /= 2;
+            }
+        }
+        return ans;
+    }
+};
